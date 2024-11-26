b99e27cc774e3e1122a4047537fb1933c70ba3a8
Oksana <oksa.osidach@gmail.com>
2024-11-26 21:30:18 +0100

    login credentials check

--- app/src/main/java/com/example/reg_w/LoginActivity.kt
+++ app/src/main/java/com/example/reg_w/LoginActivity.kt
@@ -2,10 +2,26 @@ package com.example.reg_w
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
 
 class LoginActivity : AppCompatActivity() {
+    val credentialsManager = CredentialsManager()
+    val editMailLayout: TextInputLayout
+        get() = findViewById(R.id.editMail)
+
+    val editPasswordLayout : TextInputLayout
+        get() = findViewById<TextInputLayout>(R.id.editPassword)
+
+    val editMailContent: TextInputEditText
+        get() = findViewById(R.id.editMailContent)
+
+    val submitButton: Button
+        get() = findViewById(R.id.signInButton)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +34,41 @@ class LoginActivity : AppCompatActivity() {
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
             startActivity(intent)
             finish()
+        }
+
+        submitButton.setOnClickListener {
+            val email = editMailContent.text.toString()
+            val password = findViewById<TextInputEditText>(R.id.editPasswordContent).text.toString()
+
+            var isValid = true
+
+            if (!credentialsManager.isEmailValid(email)) {
+                editMailLayout.error = "Invalid email format"
+                isValid = false
+            } else if (!credentialsManager.isLoginEmailValid(email)) {
+                editMailLayout.error = "Invalid email"
+                isValid = false
+            } else{
+                editMailLayout.isErrorEnabled = false
+            }
+
+            if (password.isEmpty()) {
+                editPasswordLayout.error = "Password cannot be empty"
+                isValid = false
+            } else if (!credentialsManager.isLoginPasswordValid(password)) {
+                editPasswordLayout.error = "Invalid password"
+                isValid = false
+            } else{
+                editPasswordLayout.isErrorEnabled = false
+            }
+
+            if(isValid){
+                val intent = Intent(this, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+                finish()
+            }
+
         }
     }
 }
