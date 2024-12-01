eee78ca8c1cd95f909137c076ed4e5b23f3b95ad
Oksana <oksa.osidach@gmail.com>
2024-12-01 22:32:54 +0100

    tests: removing unecessary comments

--- app/src/test/java/com/example/reg_w/CredentialsManagerTest.kt
+++ app/src/test/java/com/example/reg_w/CredentialsManagerTest.kt
@@ -4,7 +4,6 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class CredentialsManagerTest {
-    // Test empty email
     @Test
     fun givenEmptyEmail_thenReturnFalse() {
         val credentialsManager = CredentialsManager()
@@ -14,7 +13,6 @@ class CredentialsManagerTest {
         assertEquals(false, isEmailValid)
     }
 
-    // Test wrong email format
     @Test
     fun givenWrongEmail_thenReturnFalse() {
         val credentialsManager = CredentialsManager()
@@ -24,7 +22,6 @@ class CredentialsManagerTest {
         assertEquals(false, isEmailValid)
     }
 
-    // Test proper email
     @Test
     fun givenProperEmail_thenReturnTrue() {
         val credentialsManager = CredentialsManager()
@@ -34,7 +31,6 @@ class CredentialsManagerTest {
         assertEquals(true, isEmailValid)
     }
 
-    // Test empty password
     @Test
     fun givenEmptyPassword_thenReturnFalse() {
         val credentialsManager = CredentialsManager()
@@ -44,7 +40,6 @@ class CredentialsManagerTest {
         assertEquals(false, isPasswordValid)
     }
 
-    // Test filled password
     @Test
     fun givenFilledPassword_thenReturnTrue() {
         val credentialsManager = CredentialsManager()
